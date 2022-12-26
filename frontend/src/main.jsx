ee4f3287b095274bcfe09b4855aa5c71aef649bc
James Yoo <jyoo3607@gmail.com>
2022-12-26 01:21:23 -0800

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../scss/index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./index";
+import About from "../pages/about";
+import Contact from "../pages/contact";
+import Work from "../pages/work";
+import Work from "../pages/about";
+
+import ErrorPage from "./routes/404";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+  {
+    path: "work",
+    element: <Work />,
+  },
+  {
+    path: "work/:workId",
+    element: <WorkPiece />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
