c9cacb17e569977d5bc92632ce5743d7835d4a47
James Yoo <jyoo3607@gmail.com>
2023-01-03 14:56:39 -0800

    update

--- frontend/src/routes/work_piece.jsx
+++ frontend/src/routes/work_piece.jsx
@@ -28,11 +28,31 @@ const work_piece = () => {
       <section id="work_layout" className="container-fluid">
         {detail.map((data, index) => (
           <div className="work_layout__container" key={index}>
+            {/* header */}
             <div className="work_header">
               <Link to="/work">
                 <h5>Works &gt;</h5>
-                <h4>{data.title}</h4>
               </Link>
+              <h4>{data.title}</h4>
+            </div>
+            {/* cover image */}
+            <div className="work__cover_image">
+              <img
+                src={data.cover_image}
+                alt={data.cover_image_alt}
+                className="img-responsive"
+              />
+            </div>
+            {/* tags */}
+            <div className="work__tags">
+              <div className="work_type">
+                <span className="badge">Type</span>
+                <span className="badge_desc">{data.type}</span>
+              </div>
+              <div className="work_skills">
+                <span className="badge">Skills</span>
+                <span className="badge_skills">{data.skills}</span>
+              </div>
             </div>
           </div>
         ))}
